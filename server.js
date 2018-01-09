b0e06c64ff81cfb225df18b216a09ed27da72fee
Deqing Huang <hdqdevin@gmail.com>
2018-01-09 11:19:49 +0000

    anonymous function

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 let http = require('http')
 
 let start = function() {
-  let onRequest = function(request, response) {
-    response.writeHead(200, {'Content-type': 'text/plain'})
-    response.write('Hi version 2.1')
-    response.end()
+  http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-type': 'text/plain'})
+    res.write('Hi version 2.11')
+    res.end()
     console.log('received a request')
-  }
-
-  http.createServer(onRequest).listen(5000)
+  }).listen(5000)
 }
 
 exports.start = start
